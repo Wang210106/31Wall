4ffa6acb16c7a0cf152d39fcdefda6c046c934a1
Wang210106 <wang21010620080119@163.com>
2025-02-02 22:04:38 +0800

    Initial Commit

---
+++ pages/my/my.js
@@ -0,0 +1,80 @@
+// pages/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items0: [
+        { id : 0, text : "我的发帖"},
+        { id : 1, text : "我的点赞"},
+        { id : 2, text : "浏览历史"},
+    ],
+    items1: [
+        { id : 0, text : "设置"},
+        { id : 1, text : "开发者名单"},
+        { id : 2, text : "支持一下"},
+        { id : 3, text : "问题反馈＆联系我们"},
+    ],
+    headImage: "/image/hd1.png",
+    userName: "车柏乐",
+    class: 10,
+    session: 26
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
