420f502e10c25b260ce01c6873667a10c3b09105
Wang210106 <wang21010620080119@163.com>
2025-02-03 13:20:34 +0800

    Commit

--- pages/my/my.js
+++ pages/my/my.js
@@ -6,15 +6,15 @@ Page({
    */
   data: {
     items0: [
-        { id : 0, text : "我的发帖"},
-        { id : 1, text : "我的点赞"},
-        { id : 2, text : "浏览历史"},
+        { id : 0, text : "我的发帖", iconfont : "icon-wofadetiezi"},
+        { id : 1, text : "我的点赞", iconfont : "icon-24px"},
+        { id : 2, text : "浏览历史", iconfont : "icon-lishi"},
     ],
     items1: [
-        { id : 0, text : "设置"},
-        { id : 1, text : "开发者名单"},
-        { id : 2, text : "支持一下"},
-        { id : 3, text : "问题反馈＆联系我们"},
+        { id : 0, text : "设置", iconfont : "icon-shezhi"},
+        { id : 1, text : "开发者名单", iconfont : "icon-kaifazheguanli"},
+        { id : 2, text : "支持一下", iconfont : "icon-juankuanmingxi"},
+        { id : 3, text : "问题反馈＆联系我们", iconfont : "icon-jishuzhichi"},
     ],
     headImage: "/image/hd1.png",
     userName: "车柏乐",
